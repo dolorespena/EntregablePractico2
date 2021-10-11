788bc9cd29a9b0548f3f1a7ba8053759095113e7
Mauricio Oruezabal <moruezabal@gmail.com>
2021-10-11 12:28:02 -0300

    board.draw() without canvas parameter

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ spanDiscs.innerHTML = rangeDiscs.value + " en línea"; //Seteo por defecto en 4 
 let discsToWin = Number(rangeDiscs.value); 
 
 let board = new Board(discsToWin + 2 , discsToWin + 3, ctx);
-board.draw(canvas);
+board.draw();
 console.log(board.getCells())
 
 
@@ -20,5 +20,51 @@ rangeDiscs.addEventListener("change", ()=> {
     spanDiscs.innerHTML = rangeDiscs.value + " en línea";
     discsToWin = Number(rangeDiscs.value); 
     board = new Board(discsToWin + 2 , discsToWin + 3, ctx);
-    board.draw(canvas);
+    clearCanvas();
+    board.draw();
 })
+
+// Selección de ficha
+
+function onMouseDown(e){
+    isMouseDown = true;
+
+    if(lastClickedFigure != null){
+        lastClickedFigure.setResaltado(false);
+        lastClickedFigure = null;
+    }
+
+    let clickFig = findClickedFigure(e.layerX, e.layerY);
+    if (clickFig != null){
+        clickFig.setResaltado(true);
+        lastClickedFigure = clickFig;
+
+    }
+    drawDiscs();
+}
+
+function onMouseMove(e){
+    if (isMouseDown && lastClickedFigure != null){
+        lastClickedFigure.setPosition(e.layerX, e.layerY);
+        drawDiscs();
+    }
+}
+
+function onMouseUp(){
+    isMouseDown = false;
+}
+
+function clearCanvas(){
+    ctx.fillStyle = '#F8F8FF';
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+}
+
+function findClickedFigure(x, y){
+    for (let i = 0; i< discs.length; i++)
+        if (discs[i].isPointerInside(x, y))
+            return discs[i];
+}
+
+// canvas.addEventListener('mousedown', onMouseDown, false);
+// canvas.addEventListener('mouseup', onMouseUp, false);
+// canvas.addEventListener('mousemove', onMouseMove, false);
