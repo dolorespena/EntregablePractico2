452cbca57d1642cc3446e881d85d1b0d8556bd0a
Mauricio Oruezabal <moruezabal@gmail.com>
2021-10-14 01:13:38 -0300

    select player's colors & start game

--- js/main.js
+++ js/main.js
@@ -7,54 +7,84 @@ import Disc from "./Disc.js";
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let btnStart = document.getElementById("starGame");
+let btnStart = document.getElementById("startGame");
 
+let msgSelectPlayer = document.getElementById("selectPlayer");
 let rangeDiscs = document.getElementById("rangeDiscs"); //El selector de cantidad de fichas
 let spanDiscs = document.getElementById("showCantDiscs"); //Donde se muestra la cantidad de fichas seleccionadas
 spanDiscs.innerHTML = rangeDiscs.value + " en línea"; //Seteo por defecto en 4 línea
+
 
 let discsToWin = Number(rangeDiscs.value);  // Cantidad de fichas consecutivas para ganar
 
 let isMouseDown = false;
 let lastClickedFigure = null;
 
-let imagesDisc = document.querySelectorAll("img"); // Devuelve una lista de img
-console.log(imagesDisc)
-
-imagesDisc.forEach(image => {
-    image.addEventListener("click", (event)=>{
-        let selectImg = event.target;
-        if(selectImg.classList.contains('borderP1'))
-            selectImg.classList.add('borderP2');
-        else
-            selectImg.classList.add('borderP1');
-    })
-})
-/* imagesDisc.addEventListener("click", (event)=>{
-    console.log(event.target.tagName);
-
-
-}) */
-
-let random1 = Math.floor( Math.random() * imagesDisc.length);
-let random2 = Math.floor( Math.random() * imagesDisc.length);
-
-
 let board = new Board(discsToWin + 2 , discsToWin + 3, ctx); //Creación del tablero
-let cellP1 = new Cell(100,250, false, imagesDisc.item(random1), ctx);
-let cellP2 = new Cell(100,410, false, imagesDisc.item(random2), ctx);
+let cellP1 = new Cell(100,250, true, null, ctx);
+let cellP2 = new Cell(100,410, true, null, ctx);
 
 drawGameElements();
 
 let grippeableDiscs = [] ; //listado de fichas que se pueden agarrar
-grippeableDiscs.push(cellP1, cellP2);
 
+let imagesDisc = document.querySelectorAll("img"); // Devuelve una lista de img
+console.log(imagesDisc)
+
+imagesDisc.forEach(image => {
+    image.addEventListener("click", setPlayer);
+})
+
+function setPlayer(evt){
+
+    let elementsWithClass = document.getElementsByClassName('borderP1').length + document.getElementsByClassName('borderP1').length;
+    if (elementsWithClass == 2){
+        removeListenerAllButtons();
+    }
+
+    let selectImg = evt.target;
+    
+    msgSelectPlayer.innerHTML = 'Elegir jugador 2:';
+    selectImg.removeEventListener('click', setPlayer);
+    
+    if (document.getElementsByClassName('borderP1').length != 0){
+        cellP2 = new Cell(100,410, false, selectImg, ctx);
+        cellP2.print(ctx);
+        selectImg.classList.add('borderP2');
+        msgSelectPlayer.innerHTML = '¡Estamos listos!';  
+        btnStart.classList.toggle('show');     
+    }
+    else{
+        cellP1 = new Cell(100,250, false, selectImg, ctx);
+        cellP1.print(ctx);
+        selectImg.classList.add('borderP1');    
+    }
+}
+
+function removeListenerAllButtons(){
+    imagesDisc.forEach(image => {
+        image.removeEventListener("click", setPlayer);
+    })
+}
 
 rangeDiscs.addEventListener("change", ()=> {
 
     spanDiscs.innerHTML = rangeDiscs.value + " en línea";
     discsToWin = Number(rangeDiscs.value); 
     board = new Board(discsToWin + 2 , discsToWin + 3, ctx);
+
+    cellP1 = new Cell(100,250, true, null, ctx);
+    cellP2 = new Cell(100,410, true, null, ctx);
+
+    grippeableDiscs = [];
+
+    imagesDisc.forEach(image => {
+        image.addEventListener("click", setPlayer);
+        image.classList.remove('borderP1', 'borderP2');
+    })
+
+    msgSelectPlayer.innerHTML = 'Elegir jugador 1:';
+
     drawGameElements();
 })
 
@@ -95,6 +125,10 @@ function onMouseUp(e){
 }
 
 function startGame(){
+    grippeableDiscs.push(cellP1, cellP2);
+    btnStart.classList.toggle('show'); 
+    msgSelectPlayer.innerHTML = '';
+
     canvas.addEventListener('mousedown', onMouseDown, false);
     canvas.addEventListener('mouseup', onMouseUp, false);
     canvas.addEventListener('mousemove', onMouseMove, false);
